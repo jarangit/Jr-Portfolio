497421ca3f79e8400f94446ec2b37527dc37dc0d
jarangit <jaran.dch@gmail.com>
2020-06-04 10:23:10 +0700

    add my project

--- Component/ShowImage.js
+++ Component/ShowImage.js
@@ -1,42 +1,45 @@
 import axios from "axios"
 import { useEffect, useState } from 'react'
 
-const  ShowImage = async () => {
+const  ShowImage = (props) => {
+  console.log(props.assetsId)
+  const [data, setData] = useState([]);
+    const [load, setLoad] = useState(false);
+    const [error, setError] = useState('');
 
-  const [data, setData] = useState({ hits: [] });
-  useEffect(()=>{
-    loadData()
-  }, [])
+    useEffect(() => {
+        axios.get(`https://cdn.contentful.com/spaces/${process.env.SPACE_ID}/environments/master/assets/${props.assetsId}?access_token=${process.env.ACCESS_TOKEN}&content_type=myProject`)
+            .then(res => {
+              setData(res.data);
+                setLoad(true);
+            })
+            .catch(err => {
+                setError('No Data');
+                setLoad(true)
+            })
+    }, []);
+    
+    
+    if (load) {
+        return(
+          <div>
+            {console.log(data.sys.id)}
+            <img  src =  {data.fields.file.url} />
+          </div>
+        )
+    } else {
+        return (
+            <div>
+                Loading...
+            </div>
+        );
+    }
+};
 
-  const loadData = async () => {
-    const response  = await fetch( `https://cdn.contentful.com/spaces/${process.env.SPACE_ID}/environments/master/assets/4NAR7Mm1KAmYJUS8qQYbUm?access_token=${process.env.ACCESS_TOKEN}&content_type=myProject`)
-    const data = await response.json()
-    setData(data)
-    console.log(data.fields)
-  }
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     const result = await axios(
-  //       `https://cdn.contentful.com/spaces/${process.env.SPACE_ID}/environments/master/assets/4NAR7Mm1KAmYJUS8qQYbUm?access_token=${process.env.ACCESS_TOKEN}&content_type=myProject`,
-  //     );
- 
-  //     setData(result.data);
-  //   };
- 
-  //   fetchData();
-  // }, []);
- 
-//  fetch(`https://cdn.contentful.com/spaces/${process.env.SPACE_ID}/environments/master/assets/4NAR7Mm1KAmYJUS8qQYbUm?access_token=${process.env.ACCESS_TOKEN}&content_type=myProject`)
-//   .then(response => response.json())    // one extra step
-//   .then(data => {
-//     SetDataState(data)
-//   })
-    return(
-      <div> {data.fields.file}
-      </div>
-    )
-  }
-  
+
+
+  //  const response  =  fetch( `https://cdn.contentful.com/spaces/${process.env.SPACE_ID}/environments/master/assets/4NAR7Mm1KAmYJUS8qQYbUm?access_token=${process.env.ACCESS_TOKEN}&content_type=myProject`)
+
  
 
   export default ShowImage
