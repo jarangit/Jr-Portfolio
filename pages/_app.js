b9759c600c3bd2478dc45f09e019923b25ed50a8
jarangit <52028847+jarangit@users.noreply.github.com>
2021-03-20 12:52:52 +0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,8 @@
 import './styles.css'
 import { config } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
+import { ServerStyleSheet } from 'styled-components';
+
 config.autoAddCss = false 
 
 
